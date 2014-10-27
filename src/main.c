dd06cad2a3a3bb80252b23497fa798673add3c7f
bitdust <bitdust@users.noreply.github.com>
2014-10-27 23:45:44 +0800

    修改xclient主程序为动态链接库，以全局变量形式返回登陆日志

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "pcaphelper.h"
 
 #ifndef WIN32
@@ -8,79 +9,67 @@
 #else
 #include <winsock2.h>
 #include <windows.h>
+#include <process.h>
 #pragma comment(lib, "ws2_32.lib")
 #endif
 
+
+
+//log work
+extern char logbuffer[100] = ""; //store the log 
+static int isReaded = 1;
+extern _declspec(dllexport) char *read_log();
+void write_log();
+
+//auth thread work
+DWORD WINAPI auth_thread_func();
+static 	char sUserName[100];
+static 	char sPassword[100];
+static 	char sDeviceName[100];
+extern int stop_flag = 1;
+extern _declspec(dllexport) void start_auth_thread(const char *UserName, const char *Password, const char *DeviceName);
+extern _declspec(dllexport) void stop_auth_thread();
+//from auth.c
 extern int Authentication(const char *UserName, const char *Password, const char *DeviceName);
 
-int main(int argc, char *argv[])
+
+DWORD WINAPI auth_thread_func()
 {
-	char *UserName = NULL;
-	char *Password = NULL;
-	char *DeviceName = NULL;
-	char buf[256];
-
-	if (argc < 3 || argc > 4) {
-		char *appname;
-#if defined(WIN32)
-		int len, i;
-		appname = argv[0];
-		len = strlen(appname);
-
-		for (i = len - 1; i > 0; i--) {
-			if (appname[i] == '\\' || appname[i] == '/') {
-				appname = appname + i + 1;
-				break;
-			}
-		}
-#else
-		appname = argv[0];
-#endif
-		fprintf(stderr, "命令行参数错误！\n");
-		fprintf(stderr,	"正确的调用格式例子如下：\n");
-		fprintf(stderr,	"    %s username password\n", appname);
-		fprintf(stderr,	"    %s username password eth0\n", appname);
-		fprintf(stderr,	"    %s username password eth1\n", appname);
-		fprintf(stderr, "(注：若不指明网卡，默认情况下将会进行网卡选择)\n");
-		exit(-1);
-	} else if (argc == 4) {
-		DeviceName = argv[3]; // 允许从命令行指定设备名
-	} else {
-		// 缺省情况下使用的设备
-		char errbuf[PCAP_ERRBUF_SIZE];
-		pcap_if_t *alldevs;
-		pcap_if_t *dev;
-		char key;
-
-		if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-			fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
-			exit(1);
-		}
-
-		printf("Select an adapter by input Y\n");
-
-		for (dev = alldevs; dev != NULL; dev = dev->next) {
-			printf("%s (%s)\n", dev->name, dev->description);
-			key = getchar();
-			if (key == 'Y' || key == 'y') {
-				strcpy(buf, dev->name);
-				DeviceName = buf;
-				break;
-			}
-		}
-
-		pcap_freealldevs(alldevs);
-	}
-
-	if (!DeviceName) {
-		printf("Please select an adapter!\n");
-		exit(-1);
-	}
-
-	UserName = argv[1];
-	Password = argv[2];
-
-	Authentication(UserName, Password, DeviceName);
-	
+	Authentication(sUserName, sPassword, sDeviceName);
 	return 0;
+}
+void start_auth_thread(const char *UserName, const char *Password, const char *DeviceName)
+{
+	stop_flag = 0;
+	strcpy(sUserName, UserName);
+	strcpy(sPassword, Password);
+	strcpy(sDeviceName, DeviceName);
+	printf("device = %s\n", sDeviceName);
+	CreateThread(NULL, 0, auth_thread_func, NULL, 0, NULL);
+	return;
+}
+void stop_auth_thread()
+{
+	stop_flag = 1;
+	printf("\nstoped!\n");
+	return;
+}
+
+char *read_log()
+{
+	if (!isReaded)
+	{
+		isReaded = 1;
+		return logbuffer;	
+	}
+	else return "";
+}
+void write_log(char* info)
+{
+	if (strlen(info) < 99)
+	{
+		strcpy(logbuffer, info);
+		isReaded = 0;
+	}
+	return;
 }
